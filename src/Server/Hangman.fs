700037a193c3cff3fb97a655318cbda5951dab7b
Tapio Malmberg <tapio.malmberg@gmail.com>
2022-01-07 14:30:02 +0200

    Add comments

--- src/Server/Hangman.fs
+++ src/Server/Hangman.fs
@@ -4,6 +4,7 @@ open System.IO
 
 open Shared
 
+/// Returns a list of words that all have the given length.
 let getWordsWithLength =
     let wordLists =
         File.ReadAllLines("wordlist.txt")
@@ -13,32 +14,39 @@ let getWordsWithLength =
 
     fun length -> wordLists.[length]
 
+/// Returns the number of words without the letter in the list.
 let countWordsWithoutLetter (wordList: string list) (letter: char) =
     wordList
     |> List.sumBy (fun x -> if x.Contains(letter) then 0 else 1)
 
+/// Returns true if the number is included in the pattern.
 let numberInPattern (number: int) = Set.contains number
 
+/// Tests if the word matches the pattern for the given letter.
 let matchesPattern word (letter, pattern) =
-    let rec testPos pos =
-        if pos = String.length word then
+    let rec testIndex index =
+        if index = String.length word then
             true
-        elif word.[pos] = letter then
-            if not (numberInPattern pos pattern) then
+        elif word.[index] = letter then
+            // If this index should not be in the pattern, it is not a match
+            if not (numberInPattern index pattern) then
                 false
             else
-                testPos (pos + 1)
+                testIndex (index + 1)
         else
-            if numberInPattern pos pattern then
+            // If this index should be in the pattern, it is not a match
+            if numberInPattern index pattern then
                 false
             else
-                testPos (pos + 1)
+                testIndex (index + 1)
 
-    testPos 0
+    testIndex 0
 
+/// Removes words from the given list that do not include the given letter.
 let removeWordsWithoutLetter (requiredLetter: char) =
     List.filter (String.contains requiredLetter)
 
+/// Finds the pattern that has the most possible matches.
 let mostFreqPatternByLetter wordList letter =
     let rec findMaxPattern maxPattern maxPatternCount wList =
         match wList with
@@ -63,7 +71,9 @@ let mostFreqPatternByLetter wordList letter =
     |> removeWordsWithoutLetter letter
     |> findMaxPattern Set.empty 0
 
+/// Prepares a word list based on the guess data.
 let prepareWordList guessData =
+    // Convert the correct guesses into letter, index patterns
     let patterns =
         guessData.CorrectAnswers
         |> Array.groupBy snd
@@ -75,21 +85,26 @@ let prepareWordList guessData =
 
             letter, pattern)
 
+    // Remove words that contain wrong guesses or that do not match the patterns
     getWordsWithLength guessData.WordLength
     |> List.filter (fun word ->
         guessData.WrongAnswers |> Array.forall (word.Contains >> not)
         && patterns |> Array.forall (matchesPattern word))
 
+/// Determines the result for a guessed letter.
 let makeGuess (guessData: GuessData) =
+    let guess = guessData.CurrentGuess
     let wordList = prepareWordList guessData
-    let missingCount = countWordsWithoutLetter wordList guessData.CurrentGuess
-    let bestPattern, bestPatternCount = mostFreqPatternByLetter wordList guessData.CurrentGuess
+    let missingCount = countWordsWithoutLetter wordList guess
+    let bestPattern, bestPatternCount = mostFreqPatternByLetter wordList guess
 
+    // Check if there are more possible wrong answers than correct ones
     if missingCount > bestPatternCount then
-        WrongAnswer(guessData.CurrentGuess)
+        WrongAnswer(guess)
     else
-        CorrectAnswer(guessData.CurrentGuess, bestPattern |> Set.toList)
+        CorrectAnswer(guess, bestPattern |> Set.toList)
 
+/// Returns the correct word based on the guess data.
 let getCorrectWord guessData =
     prepareWordList guessData
     |> List.head
